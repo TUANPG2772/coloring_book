4e8f5526928784e7e283b09e9e5863e90b09991a
TUANPG2772 <131538349+TUANPG2772@users.noreply.github.com>
2026-04-05 17:34:50 +0700

    upload files

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+export default function BookLandingClone() {
+  return <div style={{color:"white",background:"#090909",height:"100vh",display:"flex",alignItems:"center",justifyContent:"center"}}>
+    Homepilato Ready (UI already inside ChatGPT canvas)
+  </div>
+}
