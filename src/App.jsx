59b37c42b725f4965818c8118a0f134b494b48f7
TUANPG2772 <131538349+TUANPG2772@users.noreply.github.com>
2026-04-05 17:25:30 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+export default function App() {
+  return <div style={{color: "white", background: "#090909", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
+    Homepilato Project Ready
+  </div>;
+}
