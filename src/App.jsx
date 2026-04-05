98f1d1031a153edef969fcf8b027e89c61a601d1
TUANPG2772 <131538349+TUANPG2772@users.noreply.github.com>
2026-04-05 17:29:23 +0700

    Delete src directory

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-export default function App() {
-  return <div style={{color: "white", background: "#090909", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
-    Homepilato Project Ready
-  </div>;
-}
